cc6fee3118f7093719e3e1a4c8f395fa42f693b1
anarodriguez <anarodriguez4502@gmail.com>
2026-05-27 17:09:05 -0600

    Automatizando migraciones en wsgi.py

--- control_escolar/wsgi.py
+++ control_escolar/wsgi.py
@@ -1,4 +1,8 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_escolar.settings')
+django.setup()
+from django.core.management import call_command
+call_command('migrate')
 application = get_wsgi_application()
